4ab89f6bccf33d3b5a1ae3655327b24a30be0c10
Jeremy <jsheridanwells@gmail.com>
2018-03-04 16:01:00 -0600

    simple hourglass program

--- hour-glass/hourGlass.js
+++ hour-glass/hourGlass.js
@@ -1,45 +1,37 @@
-'use strict';
-let five = require('johnny-five');
+// get johnny five
+let five = require("johnny-five");
+// get system sleep 
+let sleep = require("system-sleep");
 
 let board = new five.Board();
 
-board.on('ready', () => {
-  let tilt = new five.Switch(13);
-  let first = 2;
-  let last = 8;
-  let leds = assignLeds(first, last);
-  let prevTime = 0;
-  let currentTime = board.millis();  
-  let interval = 600000;
-  let tiltState = 0;
-  let prevTiltState = 0;
-
-  if ((currentTime - prevTime) > interval) {
-    prevTime = currentTime;
-    leds[first].on();
-    first++
-    if (first === last +1) {
-      leds.forEach(led => led.off());
-    }
-  } 
-
-  tiltState = board.digitalRead(13, val => val);
-
-  if (tiltState !== prevTiltState){
-    ledsOff(leds);
-    first = 2;
-  }
-
-  prevTiltState = tiltState;
-  
+// run program on connected Arduino
+board.on("ready", () => {
+  let leds = setLeds(2, 7);
+  let button = new five.Button(8);
+  button.on('press', () => leds.forEach(led => onAndWait(led)));
 });
 
-const assignLeds = (first, last) => {
+// creates an array of leds with pin assigned
+// leds must be in order from first to last`
+const setLeds = (first, last) => {
   let arr = [];
-  for (let i = first; i <= last; i++){
-    arr.push(new five.Led(i));
+  for (let i = first; i <= last; i++) {
+    let led = new five.Led(i);
+    arr.push(led);
   }
   return arr;
 };
 
-const ledsOff = (ledArr) => ledArr.forEach(led => led.off());
+// turns on an led, waits 5 minutes, then turns it off
+// passed into forEach method on array of leds, turns each one
+// on and off individually
+const onAndWait = (thisLed) => {
+  thisLed.on();
+  sleep(minutes(5));
+  thisLed.off();
+};
+
+// converts minutes to milliseconds
+// so sleep() method is more readable
+const minutes = mins => mins * 60000;
