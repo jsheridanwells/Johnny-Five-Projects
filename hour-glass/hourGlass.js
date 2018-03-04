44b81ce426eee0463526a7f46b1b33b932cc221f
Jeremy <jsheridanwells@gmail.com>
2018-03-04 14:06:21 -0600

    tilt methods created

--- hour-glass/hourGlass.js
+++ hour-glass/hourGlass.js
@@ -4,8 +4,16 @@ var five = require('johnny-five');
 var board = new five.Board();
 
 board.on('ready', () => {
-  var ledArr = new five.Leds([2,3,4,5,6,7]);
-  board.repl.inject({ leds: ledArr });
-  ledArr.blink();
+  // var ledArr = new five.Leds([2,3,4,5,6,7]);
+  // board.repl.inject({ leds: ledArr });
+  // ledArr.blink();
+
+  var tilt = new five.Button(8);
+
+  tilt.on('down', () => console.log('down'));
+  tilt.on('up', () => console.log('up'));
+
+
+
 });
 
